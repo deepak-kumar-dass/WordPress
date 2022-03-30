dd3dacdfdf39ad2537f2fd565df231b4548dfcb4
deepak-kumar-dass <96599686+deepak-kumar-dass@users.noreply.github.com>
2022-03-31 02:15:07 +0530

    Update create-a-custom-top-level-menu-page.php

--- create-a-custom-top-level-menu-page.php
+++ create-a-custom-top-level-menu-page.php
@@ -1,22 +1,22 @@
 <!-- Create a custom top-level menu page : Add this code in functions.php  -->
 
-add_action( 'admin_menu', 'misha_menu_page' );
+add_action( 'admin_menu', 'deep_menu_page' );
 
-function misha_menu_page() {
+function deep_menu_page() {
 
 	add_menu_page(
 		'My Page Title', // page <title>Title</title>
 		'My Page', // menu link text
 		'manage_options', // capability to access the page
-		'misha-slug', // page URL slug
-		'misha_page_content', // callback function /w content
+		'deep-slug', // page URL slug
+		'deep_page_content', // callback function /w content
 		'dashicons-star-half', // menu icon
 		5 // priority
 	);
 
 }
 
-function misha_page_content(){
+function deep_page_content(){
 
 	echo 'What is up?';
 
