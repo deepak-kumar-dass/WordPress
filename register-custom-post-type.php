6ff792699707386974caddc11a4b308bf5c291a0
deepak-kumar-dass <96599686+deepak-kumar-dass@users.noreply.github.com>
2022-03-31 02:05:22 +0530

    Create register-custom-post-type.php

---
+++ register-custom-post-type.php
@@ -0,0 +1,56 @@
+// Register Custom Post Type : TV Schedule (Note: Replace "Tv Schedule" with you own post type name eg: Books,Movies,Audios etc.)
+<!-- Add This piece of code in functions.php -->
+function custom_post_type_tv_schedule() {
+
+  $labels = array(
+    'name'                  => _x( 'Tv Schedule', 'Post Type General Name', 'text_domain' ),
+    'singular_name'         => _x( 'Tv Schedule', 'Post Type Singular Name', 'text_domain' ),
+    'menu_name'             => __( 'Tv Schedule', 'text_domain' ),
+    'name_admin_bar'        => __( 'Tv Schedule', 'text_domain' ),
+    'archives'              => __( 'Item Archives', 'text_domain' ),
+    'attributes'            => __( 'Item Attributes', 'text_domain' ),
+    'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+    'all_items'             => __( 'All Tv Schedules', 'text_domain' ),
+    'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+    'add_new'               => __( 'Add New Tv Schedule', 'text_domain' ),
+    'new_item'              => __( 'New Item', 'text_domain' ),
+    'edit_item'             => __( 'Edit Item', 'text_domain' ),
+    'update_item'           => __( 'Update Item', 'text_domain' ),
+    'view_item'             => __( 'View Item', 'text_domain' ),
+    'view_items'            => __( 'View Items', 'text_domain' ),
+    'search_items'          => __( 'Search Item', 'text_domain' ),
+    'not_found'             => __( 'Not found', 'text_domain' ),
+    'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+    'featured_image'        => __( 'Featured Image', 'text_domain' ),
+    'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+    'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+    'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+    'items_list'            => __( 'Items list', 'text_domain' ),
+    'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+    'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+  );
+  $args = array(
+    'label'                 => __( 'Tv Schedule', 'text_domain' ),
+    'description'           => __( 'Post Type Description', 'text_domain' ),
+    'labels'                => $labels,
+    'supports'              => array( 'title', 'editor', 'thumbnail', 'comments', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats' ),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 5,
+    'menu_icon'             => 'dashicons-video-alt3',
+    'show_in_admin_bar'     => true,
+    'show_in_nav_menus'     => true,
+    'can_export'            => true,
+    'has_archive'           => true,
+    'exclude_from_search'   => false,
+    'publicly_queryable'    => true,
+    'capability_type'       => 'page',
+  );
+  register_post_type( 'tv-schedule', $args );
+
+}
+add_action( 'init', 'custom_post_type_tv_schedule', 0 );
