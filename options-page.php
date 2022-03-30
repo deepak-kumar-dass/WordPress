ba700453e1b862c1159da7458713a4c34f56d605
deepak-kumar-dass <96599686+deepak-kumar-dass@users.noreply.github.com>
2022-03-31 02:21:18 +0530

    Update options-page.php

--- options-page.php
+++ options-page.php
@@ -20,3 +20,47 @@ function deep_page_content(){
 	echo 'Welcome to options page!';
 
 }
+
+<!-- Complete code :: start -->
+
+add_action( 'admin_init',  'deep_register_setting' );
+
+function misha_register_setting(){
+
+	register_setting(
+		'deep_settings', // settings group name
+		'homepage_text', // option name
+		'sanitize_text_field' // sanitization function
+	);
+
+	add_settings_section(
+		'some_settings_section_id', // section ID
+		'', // title (if needed)
+		'', // callback function (if needed)
+		'deep-slug' // page slug
+	);
+
+	add_settings_field(
+		'homepage_text',
+		'Homepage text',
+		'deep_text_field_html', // function which prints the field
+		'deep-slug', // page slug
+		'some_settings_section_id', // section ID
+		array( 
+			'label_for' => 'homepage_text',
+			'class' => 'deep-class', // for <tr> element
+		)
+	);
+
+}
+
+function deep_text_field_html(){
+
+	$text = get_option( 'homepage_text' );
+
+	printf(
+		'<input type="text" id="homepage_text" name="homepage_text" value="%s" />',
+		esc_attr( $text )
+	);
+
+}
